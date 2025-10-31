bb6733b9bb16bf87a6b3f2ba92827af454bde6bb
kishanshettyb <kishanqr@gmail.com>
2025-10-31 13:10:10 +0530

    fix(responsive issues): responsive fix

--- src/components/productImage.tsx
+++ src/components/productImage.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import React from 'react'
-import { Button } from './ui/button'
 import Link from 'next/link'
 import { ArrowRight } from 'lucide-react'
 
@@ -21,15 +20,19 @@ function ProductImage({ imageSource, title, desc, link, btnTitle }: prodImage) {
         height={1920}
         className="w-full h-full object-cover"
       />
-      <div className="absolute z-50 bottom-15 left-10">
-        <h2 className="text-white text-3xl font-semibold mb-3">{title}</h2>
-        <p className="text-white mb-5 opacity-80">{desc}</p>
-        <Button variant="ghost" asChild className="text-white border opacity-50 hover:opacity-100">
-          <Link href={link}>
-            {btnTitle}
-            <ArrowRight />
-          </Link>
-        </Button>
+      <div className="absolute w-full z-50 bottom-4 lg:bottom-15 left-5 lg:left-10">
+        <h2 className="text-white text-md md:text-2xl lg:text-3xl font-semibold mb-0 lg:mb-3">
+          {title}
+        </h2>
+        <p className="text-white hidden md:block text-xs lg:text-md mb-5 opacity-80">{desc}</p>
+
+        <Link
+          href={link}
+          className=" inline-flex justify-start items-center gap-x-2 opacity-70 hover:opacity-100 text-white text-xs md:text-base"
+        >
+          {btnTitle}
+          <ArrowRight size="20" />
+        </Link>
       </div>
     </div>
   )
