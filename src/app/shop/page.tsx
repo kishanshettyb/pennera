243927a9636029185a10e38f83f4ac52ba3b0f84
kishanshettyb <kishanqr@gmail.com>
2025-11-07 12:32:37 +0530

    fix: fix
    fix issues

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
       <SmallBanner title="Shop" image="/banner/banner-categories.png" />
       <div className="w-full py-10 mx-auto px-4 sm:px-6 sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1560px] 2xl:max-w-[1560px] flex flex-row gap-x-10 items-start justify-start">
         <div className="hidden bg-slate-50 rounded-2xl lg:block w-1/5">
-          <Suspense fallback={<>...</>}>
+          <Suspense fallback={<></>}>
             <ShopFilters />
           </Suspense>
         </div>
